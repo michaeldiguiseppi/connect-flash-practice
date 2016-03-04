017f9addb23d55214a725c2d7aae6d96ccb39873
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-03-04 14:21:48 -0700

    Added new route and helpers for basic validation

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var email = [
+  'michael.diguiseppi@gmail.com',
+  'michael@mherman.org'
+];
+
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.post('/', function(req, res, next) {
-  console.log(req.body);
+router.post('/', validEmail, function(req, res, next) {
   res.render('index');
 });
+
+
+// def helpers
+
+function validEmail(req, res, next) {
+  if (email.indexOf(req.body.email) === -1) {
+    res.send('you fucked up');
+  } else {
+      return next();
+  }
+}
 
 module.exports = router;
