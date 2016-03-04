0a252b3414f49f70beec271c909a5f0394195b14
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-03-04 14:10:37 -0700

    Added connect-flash, basic set up

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  res.render('index');
 });
 
 module.exports = router;
